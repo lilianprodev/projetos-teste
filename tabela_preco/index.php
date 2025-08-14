c0ba1da6666fa7b1204cbcafa5df8ec3df410c44
Lilian Bernardo dos Santos <lilianprodev@gmail.com>
2025-08-13 23:09:38 -0300

    Create index.php

---
+++ tabela_preco/index.php
@@ -0,0 +1,75 @@
+<?php
+// Inclui o arquivo header.php que está na mesma pasta
+include "header.php";
+
+
+?>
+
+    <section class="section">
+        <h2>Bolos Tradicionais</h2>
+        <div class="cards-grid">
+            <div class="card">
+                <h3>15 cm</h3>
+                <p><strong>Fatias:</strong> 12 a 15</p>
+                <p class="price">R$ 150,00</p>
+            </div>
+            <div class="card">
+                <h3>20 cm</h3>
+                <p><strong>Fatias:</strong> 23 a 25</p>
+                <p class="price">R$ 250,00</p>
+            </div>
+            <div class="card">
+                <h3>25 cm</h3>
+                <p><strong>Fatias:</strong> 35 a 37</p>
+                <p class="price">R$ 290,00</p>
+            </div>
+            <div class="card">
+                <h3>30 cm</h3>
+                <p><strong>Fatias:</strong> 50 a 55</p>
+                <p class="price">R$ 340,00</p>
+            </div>
+        </div>
+    </section>
+    
+    ---
+
+    <section class="section">
+        <h2>Opções de Massas e Recheios</h2>
+
+        <div class="options-container">
+            <div class="option-card">
+                <h3>Tipos de Massa</h3>
+                <ul>
+                    <li>Massa branca</li>
+                    <li>Massa de chocolate</li>
+                    <li>Massa mista</li>
+                </ul>
+            </div>
+            <div class="option-card">
+                <h3>Recheios Simples</h3>
+                <ul>
+                    <li>Chocolate</li>
+                    <li>Doce de leite</li>
+                    <li>Beijinho</li>
+                    <li>Leite em pó</li>
+                    <li>Brigadeiro branco</li>
+                </ul>
+            </div>
+            <div class="option-card">
+                <h3>Recheios Premium</h3>
+                <ul>
+                    <li>Nozes</li>
+                    <li>Castanha</li>
+                    <li>Ameixa</li>
+                    <li>Morango</li>
+                    <li>Maracujá</li>
+                    <li>Quatro Leites</li>
+                </ul>
+            </div>
+        </div>
+        <p class="note">* Acréscimo de R$ 15,00 para Recheios de Maracujá.</p>
+    </section>
+</div>
+
+</body>
+</html>
